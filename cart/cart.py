2af69b8fe81989405c2b701e70750cb4f257a5a7
Nikita2422 <mishranikita076@gmail.com>
2024-09-24 18:43:10 +0530

    new commit

--- cart/cart.py
+++ cart/cart.py
@@ -14,16 +14,39 @@ class Cart():
         # make sure cart is avaialble on all pages of site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         # Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         
         self.session.modified = True
+
+    def cart_total(self):
+        # Get product IDs
+        product_ids = self.cart.keys()
+        # lookup those keys in our products database model
+        products = Product.objects.filter(id__in = product_ids)
+        # Get quantities
+        quantities = self.cart
+        # Start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            # COnvert key string into int 
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total
+
 
     def __len__(self):
         return len(self.cart)
@@ -32,6 +55,33 @@ class Cart():
         # Get ids from cart
         product_ids= self.cart.keys() 
         # Use ids to lookup products in database model
-        products = Product.objects.filter(id_in= product_ids)
+        products = Product.objects.filter(id__in= product_ids)
         # Return those Products
         return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return  quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+		# Get cart
+        ourcart = self.cart
+		# Update Dictionary/cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+
+        thing = self.cart
+        return thing
+    
+    def delete(self, product):
+        product_id = str(product)
+        # Delete from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
